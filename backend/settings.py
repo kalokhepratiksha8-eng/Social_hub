37a4c0a4d2f7c9eefaa3f563b1d846ae2c49cbda
Pratiksha <kalokhepratiksha8@gmail.com>
2026-06-08 17:05:40 +0530

    SQLite for render deploy

--- backend/settings.py
+++ backend/settings.py
@@ -55,15 +55,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # MySQL Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'snapgram_db',
-        'USER': 'root',
-        'PASSWORD': 'P@K!2816#P',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
